b717e0094ca5b3294f860673f73de937325dd3eb
Sneha0716 <snehamishra0013@gmail.com>
2025-12-06 20:54:37 +1300

    finished the project

---
+++ js/index.js
@@ -0,0 +1,47 @@
+
+//     let form=document.getElementById("signinForm");
+//     let msg=document.getElementById("msg")
+
+
+//     form.addEventListener("submit",function(e)
+
+// {
+//     e.preventDefault();
+//     let name=document.getElementById("username").value;
+//     let email=document.getElementById("email").value;
+//     let password=document.getElementById("password").value;
+//     let confirm=document.getElementById("confirm").value;
+
+    
+// if(name.trim()=="")
+// {
+//     alert("Name is mandatory")
+// }
+// else if(email.trim()=="")
+// {
+//     alert("Email is mandatory")
+// }
+// else if(password.trim().length<8)
+// {
+//     alert("Please enter password min of 8 chars")
+// }
+// else if(confirm!=password)
+// {
+//     alert("Password and Confirm Password should be same")
+// }
+// else{
+//     msg.innerHTML=`SignUp successful`
+//     console.log(name);
+    
+//     localStorage.setItem("username",name)
+//     localStorage.setItem("password",password)
+//     window.location.href="login.html"
+// }
+// })
+
+
+
+
+// 
+
+console.log("hello");
